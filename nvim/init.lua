f17abf620fb8866e502fa644af0fe4b7cc9d1408
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2025-01-23 23:34:43 -0800

    Add autocomplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,6 +33,27 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end,
 })
 
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+}
+cmp_mappings['<Tab>'] = nil
+cmp_mappings['<S-Tab>'] = nil
+cmp.setup({
+        completion = {
+                completeopt = 'menu,menuone,noinsert',
+        },
+        mapping = cmp_mappings,
+        sources = {
+                { name = 'nvim_lsp' },
+                { name = 'buffer' },
+        },
+})
+
 require('lspconfig').lua_ls.setup({
         settings = {
                 Lua = {
