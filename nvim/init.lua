0762d6925b2c2089e814492c1ab347446018900e
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2025-02-10 12:40:57 -0800

    Add gl mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,7 @@ vim.api.nvim_create_autocmd(
         vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
         vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
         vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
       end
     }
 )
