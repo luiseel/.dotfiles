d8330af82a6acfb50f9277b39d360b7340e36f95
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2025-02-10 13:10:34 -0800

    Show dotfiles in nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -8,6 +8,6 @@ return {
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({sort_by = "case_sensitive", view = {width = 40}, filters = {dotfiles = false}})
+    nvimtree.setup({sort_by = "case_sensitive", view = {width = 40}, filters = {dotfiles = true}})
   end
 }
