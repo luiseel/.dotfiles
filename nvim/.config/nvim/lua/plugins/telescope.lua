b5a891c5f8facd924007b0ee1e3adfb812f29f06
Luis Espinoza <79593471+luiseel@users.noreply.github.com>
2026-04-14 14:55:42 -0700

    chore: Format code

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,8 +2,7 @@ return {
   "nvim-telescope/telescope.nvim",
   tag = "0.1.8",
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    {"nvim-telescope/telescope-fzf-native.nvim", build = "make"},
+    "nvim-lua/plenary.nvim", {"nvim-telescope/telescope-fzf-native.nvim", build = "make"}
   },
   config = function()
     require('telescope').load_extension('fzf')
